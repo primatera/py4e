bdcdd6692ed077a9491babf4a6fb89cf8bf08ef2
primatera <primon.code@gmail.com>
2020-04-07 15:52:45 -0700

    Assignment Multi-table databasse - Tracks

--- py4e/py4e.py
+++ py4e/py4e.py
@@ -38,4 +38,5 @@ print("Python for Everybody - Assignments\n")
 #import assignment_geojson
 
 #Course 4: Using Databases with Python
-import assignment_counting_email_in_db
+#import assignment_counting_email_in_db
+import assignment_multitable_db
